fc0c4a3562eca61ef623141923500df446cf771a
YuriiVasiuk <yuravasiuk@gmail.com>
2017-10-24 16:40:39 -0600

    Fix module

--- syllabus.js
+++ syllabus.js
@@ -4,73 +4,91 @@
 
 var canvas = require('canvas-wrapper');
 
-function getPages(callBack) {
-    canvas.get('/api/v1/courses/375/pages', (err, response) => {
-        if (err) {
-            callBack(err);
-            return;
-        }
-        var pages = response;
-        callBack(null, pages);
-    });
-}
+module.exports = function (course, stepCallback) {
+    course.addModuleReport('setSyllabus');
 
-function findSyllabus(callBack) {
-    getPages((err, pages) => {
-        if (err) {
-            callBack(err);
-            return;
-        }
-        var found = pages.find((page) => {
-            return page.title.toLowerCase().includes('syllabus');
-        });
-        callBack(null, found);
-    });
-}
-
-function getSyllabus(syllabusUrl, callback) {
-    canvas.get(`/api/v1/courses/375/pages/${syllabusUrl}`, (err, response) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        var syllabus = response[0].body;
-        callback(null, syllabus);
-    });
-}
-
-function putSyllabus(syllabusHTML, callback) {
-    canvas.put('/api/v1/courses/375', {
-        'course[syllabus_body]': syllabusHTML,
-    }, (err1 /*, body*/ ) => {
-        if (err1) {
-            callback(err1);
-            return;
-        }
-        callback(null);
-    });
-}
-
-
-findSyllabus((error, syllabus) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    getSyllabus(syllabus.url, (getSyllabusErr, syllabusHTML) => {
-        if (getSyllabusErr) {
-            console.error(getSyllabusErr);
-            return;
-        }
-        putSyllabus(syllabusHTML, (putSyllabusErr) => {
-            // delete syllabus page
-            if (putSyllabusErr) {
-                console.log(putSyllabusErr);
+    function getPages(callBack) {
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages`, (err, response) => {
+            if (err) {
+                callBack(err);
                 return;
             }
-            canvas.delete('/api/v1/courses/375/pages/' + syllabus.url, () => {
-                console.log('this url page has been deleted ' + '/api/v1/courses/375/pages/' + syllabus.url);
+            var pages = response;
+            callBack(null, pages);
+        });
+    }
+
+    function findSyllabus(callBack) {
+        getPages((err, pages) => {
+            if (err) {
+                callBack(err);
+                return;
+            }
+            var found = pages.find((page) => {
+                return page.title.toLowerCase().includes('syllabus');
+            });
+            if (!found) {
+                callBack('No syllabus page was found.');
+            } else {
+                callBack(null, found);
+            }
+        });
+    }
+
+    function getSyllabus(syllabusUrl, callback) {
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/pages/${syllabusUrl}`, (err, response) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            var syllabus = response[0].body;
+            callback(null, syllabus);
+        });
+    }
+
+    function putSyllabus(syllabusHTML, callback) {
+        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
+            'course[syllabus_body]': syllabusHTML,
+        }, (err1) => {
+            if (err1) {
+                callback(err1);
+                return;
+            }
+            callback(null);
+        });
+    }
+
+
+    findSyllabus((error, syllabus) => {
+        if (error) {
+            course.throwErr('setSyllabus', error);
+            stepCallback(null, course);
+            return;
+        }
+        getSyllabus(syllabus.url, (getSyllabusErr, syllabusHTML) => {
+            if (getSyllabusErr) {
+                course.throwErr('setSyllabus', getSyllabusErr);
+                stepCallback(null, course);
+                return;
+            }
+            putSyllabus(syllabusHTML, (putSyllabusErr) => {
+                if (putSyllabusErr) {
+                    course.throwErr('setSyllabus', putSyllabusErr);
+                    stepCallback(null, course);
+                    return;
+                }
+                course.success(
+                    'setSyllabus',
+                    'Successfully set the Syllabus content in the Syllabus tool'
+                );
+                canvas.delete(`/api/v1/courses/${course.info.canvasOU}/pages/` + syllabus.url, () => {
+                    course.success(
+                        'setSyllabus',
+                        'Successfully deleted the Syllabus from Pages'
+                    );
+                    stepCallback(null, course);
+                });
             });
         });
     });
-});
+};
