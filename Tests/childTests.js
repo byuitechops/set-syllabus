104b9dfcd40fc4ef19fad5bfa8fd54ee26eb680a
Zachary  Williams <wil11063@byui.edu>
2018-03-27 10:12:02 -0600

    Update childTests.js

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,46 +1,16 @@
 /* Dependencies */
 const tap = require('tap');
+const canvas = require('canvas-wrapper');
 
-function g1Tests(course, callback) {
-    // Tap tests for Gauntlet 1 go here
-    tap.pass('Success! Wheee! 1');
-    // tap.fail('YOLO');
+module.exports = (course, callback) => {
+    tap.test('child-template', (test) => {
+
+        test.pass('potato');
+        test.pass('tomato');
+        test.fail('avacado');
+
+        test.end();
+    });
+
     callback(null, course);
-}
-
-function g2Tests(course, callback) {
-    // Tap tests for Gauntlet 2 go here
-    tap.pass('Success! Wheee! 2');
-    callback(null, course);
-}
-
-function g3Tests(course, callback) {
-    // Tap tests for Gauntlet 3 go here
-    tap.pass('Success! Wheee! 3');
-    callback(null, course);
-}
-
-function g4Tests(course, callback) {
-    // Tap tests for Gauntlet 4 go here
-    tap.pass('Success! Wheee! 4');
-    callback(null, course);
-}
-
-module.exports = [
-        {
-            gauntlet: 1,
-            tests: g1Tests
-        },
-        {
-            gauntlet: 2,
-            tests: g2Tests
-        },
-        {
-            gauntlet: 3,
-            tests: g3Tests
-        },
-        {
-            gauntlet: 4,
-            tests: g4Tests
-        },
-];
+};
